53766b2fb54af550e235521b6a44e9b715efdb64
bala-amypo <bala@amypo.in>
2025-12-21 16:04:21 +0000

    User commit

--- src/main/java/com/example/demo/model/Host.java
+++ src/main/java/com/example/demo/model/Host.java
@@ -1,65 +1,71 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "hosts")
 public class Host {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
+    @Column(nullable = false)
     private String hostName;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
     private String department;
+
     private String phone;
 
     public Host() {
     }
 
-    public Host(long id, String hostName,
-                String email, String department, String phone) {
-        this.id = id;
+    public Host(String hostName, String email, String department, String phone) {
         this.hostName = hostName;
         this.email = email;
         this.department = department;
         this.phone = phone;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
-    }
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getHostName() {
         return hostName;
     }
-    public void setHostName(String hostName) {
-        this.hostName = hostName;
-    }
 
     public String getEmail() {
         return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getDepartment() {
         return department;
     }
-    public void setDepartment(String department) {
-        this.department = department;
-    }
 
     public String getPhone() {
         return phone;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
