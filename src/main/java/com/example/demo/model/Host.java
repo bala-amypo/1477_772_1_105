889a2938686f03e0be77d3a28259bfb8e9a894c4
bala-amypo <bala@amypo.in>
2025-12-21 17:16:01 +0000

    User commit

--- src/main/java/com/example/demo/model/Host.java
+++ src/main/java/com/example/demo/model/Host.java
@@ -1,6 +1,10 @@
-package com.example.demo.model;
+package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
 
 @Entity
 @Table(name = "hosts")
@@ -10,63 +14,71 @@ public class Host {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @NotBlank
     private String hostName;
 
-    @Column(nullable = false, unique = true)
+    @Email
+    @NotBlank
+    @Column(unique = true)
     private String email;
 
+    @NotBlank
     private String department;
 
+    @NotBlank
     private String phone;
 
-    public Host() {
-    }
+    @OneToMany(mappedBy = "host")
+    private List<Appointment> appointments;
 
-    public Host(String hostName, String email, String department, String phone) {
-        this.hostName = hostName;
-        this.email = email;
-        this.department = department;
-        this.phone = phone;
-    }
+    @OneToMany(mappedBy = "host")
+    private List<VisitLog> visitLogs;
 
     public Long getId() {
         return id;
-    }
-
-    public String getHostName() {
-        return hostName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public String getPhone() {
-        return phone;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public String getHostName() {
+        return hostName;
+    }
+
     public void setHostName(String hostName) {
         this.hostName = hostName;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getDepartment() {
+        return department;
+    }
+
     public void setDepartment(String department) {
         this.department = department;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public List<VisitLog> getVisitLogs() {
+        return visitLogs;
     }
 }
