447ce2be22fff0563e46d2c3b13862ea3f7deac9
bala-amypo <bala@amypo.in>
2025-12-22 00:25:38 +0000

    User commit

--- src/main/java/com/example/demo/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/exception/ApiExceptionHandler.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class ApiExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleResourceNotFound(
             ResourceNotFoundException ex) {
