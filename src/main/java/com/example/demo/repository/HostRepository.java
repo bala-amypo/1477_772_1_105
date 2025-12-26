dc688449a222fb57c75dfb616562fabf48010109
bala-amypo <bala@amypo.in>
2025-12-26 13:01:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/HostRepository.java
+++ src/main/java/com/example/demo/repository/HostRepository.java
@@ -1,9 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Host;
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface HostRepository extends JpaRepository<Host, Long> {
 
