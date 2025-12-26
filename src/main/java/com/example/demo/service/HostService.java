d5795190e49f4c56081ff766e8abeda4e15738af
bala-amypo <bala@amypo.in>
2025-12-26 13:03:47 +0000

    User commit

--- src/main/java/com/example/demo/service/HostService.java
+++ src/main/java/com/example/demo/service/HostService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Host;
-
 import java.util.List;
 
 public interface HostService {
@@ -11,6 +10,4 @@ public interface HostService {
     Host getHost(Long id);
 
     List<Host> getAllHosts();
-
-    Host getHostByEmail(String email);
 }
