19e7b27278151a3e89a1a84fe92c7211fcbb5049
bala-amypo <bala@amypo.in>
2025-12-26 12:57:00 +0000

    User commit

--- src/main/java/com/example/demo/service/HostServiceImpl.java
+++ src/main/java/com/example/demo/service/HostServiceImpl.java
@@ -3,45 +3,25 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.Host;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.HostRepository;
-import com.example.demo.service.HostService;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class HostServiceImpl implements HostService {
+public class HostServiceImpl {
 
-    private final HostRepository hostRepository;
+    public HostRepository hostRepository;
 
-    public HostServiceImpl(HostRepository hostRepository) {
-        this.hostRepository = hostRepository;
+    public HostServiceImpl() {}
+
+    public Host createHost(Host h) {
+        return hostRepository.save(h);
     }
 
-    @Override
-    public Host createHost(Host host) {
-        try {
-            return hostRepository.save(host);
-        } catch (DataIntegrityViolationException ex) {
-            
-            throw new IllegalArgumentException("Email constraint violation");
-        }
-    }
-
-    @Override
     public Host getHost(Long id) {
         return hostRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Host not found"));
     }
 
-    @Override
     public List<Host> getAllHosts() {
         return hostRepository.findAll();
-    }
-
-    @Override
-    public Host getHostByEmail(String email) {
-        return hostRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("Host not found"));
     }
 }
