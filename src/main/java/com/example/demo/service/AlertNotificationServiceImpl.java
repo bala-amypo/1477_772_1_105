b1375bafb82d4e3d42039c33a90459ba2dfc0d7b
bala-amypo <bala@amypo.in>
2025-12-26 15:56:59 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertNotificationServiceImpl.java
+++ src/main/java/com/example/demo/service/AlertNotificationServiceImpl.java
@@ -1,45 +1,19 @@
-package com.example.demo.service.impl;
+public AlertNotification sendAlert(Long visitLogId) {
 
-import com.example.demo.entity.*;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.*;
+    VisitLog vl = visitLogRepository.findById(visitLogId)
+        .orElseThrow(() -> new ResourceNotFoundException("VisitLog not found"));
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-public class AlertNotificationServiceImpl {
-
-    public AlertNotificationRepository alertRepository;
-    public VisitLogRepository visitLogRepository;
-
-    public AlertNotificationServiceImpl() {}
-
-    public AlertNotification sendAlert(Long visitLogId) {
-
-        VisitLog log = visitLogRepository.findById(visitLogId)
-                .orElseThrow(() -> new ResourceNotFoundException("VisitLog not found"));
-
-        if (alertRepository.findByVisitLogId(visitLogId).isPresent()) {
-            throw new IllegalArgumentException("Alert already sent");
-        }
-
-        AlertNotification alert = new AlertNotification();
-        alert.setVisitLog(log);
-        alert.setSentTo(log.getHost().getEmail());
-        alert.setSentAt(LocalDateTime.now());
-
-        log.setAlertSent(true);
-        visitLogRepository.save(log);
-
-        return alertRepository.save(alert);
+    if (alertRepository.findByVisitLogId(visitLogId).isPresent()) {
+        throw new IllegalArgumentException("Alert already sent");
     }
 
-    public AlertNotification getAlert(Long id) {
-        return alertRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Alert not found"));
-    }
+    AlertNotification alert = new AlertNotification();
+    alert.setVisitLog(vl);
+    alert.setSentTo(vl.getHost().getEmail());
+    alert.setSentAt(LocalDateTime.now());
 
-    public List<AlertNotification> getAllAlerts() {
-        return alertRepository.findAll();
-    }
+    vl.setAlertSent(true);
+    visitLogRepository.save(vl);
+
+    return alertRepository.save(alert);
 }
