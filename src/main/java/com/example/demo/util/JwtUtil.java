3f9c6028b57b7ddb1e9b10095b2112d899478f35
bala-amypo <bala@amypo.in>
2025-12-26 15:57:14 +0000

    User commit

--- src/main/java/com/example/demo/util/JwtUtil.java
+++ src/main/java/com/example/demo/util/JwtUtil.java
@@ -1,32 +1,24 @@
-package com.example.demo.security;
-
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-
-import java.util.Date;
-
 public class JwtUtil {
 
-    public String secret;
-    public Long jwtExpirationMs;
+    private String secret;
+    private Long jwtExpirationMs;
 
     public String generateToken(String username, String role, Long userId, String email) {
-
         return Jwts.builder()
-                .setSubject(username)
-                .claim("role", role)
-                .claim("userId", userId)
-                .claim("email", email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
-                .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
-                .compact();
+            .claim("role", role)
+            .claim("userId", userId)
+            .claim("email", email)
+            .setSubject(username)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+            .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+            .compact();
     }
 
     public Jws<Claims> validateAndGetClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(secret.getBytes())
-                .build()
-                .parseClaimsJws(token);
+            .setSigningKey(secret.getBytes())
+            .build()
+            .parseClaimsJws(token);
     }
 }
