02717c94724d6b7ebbe45e2304a937dd2ff58fbc
bala-amypo <bala@amypo.in>
2025-12-26 13:02:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/VisitLogRepository.java
+++ src/main/java/com/example/demo/repository/VisitLogRepository.java
@@ -1,14 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.VisitLog;
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface VisitLogRepository extends JpaRepository<VisitLog, Long> {
 
     List<VisitLog> findByCheckOutTimeIsNull();
-
-    Optional<VisitLog> findByVisitorIdAndCheckOutTimeIsNull(Long visitorId);
 }
